7f314deaa6a6723b8aa4b27b34eb3507233ca07f
aleksandr <an5600590@gmail.com>
2021-10-13 15:23:34 +0300

    record.1

--- parser.rb
+++ parser.rb
@@ -6,16 +6,15 @@ require 'csv'
 
 puts 'Введите ссылку:'
 link = gets.chomp
-url = Curl.get(link)
-html = Nokogiri::HTML(url.body)
-products_url = html.xpath("//div/div/link[@itemprop='url']/@href")
+http = Curl.get(link)
+html = Nokogiri::HTML(http.body)
+products_data = html.xpath("//div/div/link[@itemprop='url']/@href")
 
 def save_info(name, weight, img, price)
-  weight.each_with_index do |w,index |
+  weight.each_with_index do |w, index|
     CSV.open('test.csv', 'a+') do |csv_file|
       csv_file << %w[Название Цена Вес] if csv_file.count.zero?
       csv_file << ["#{name&.text} - #{w&.text}", price[index]&.text.to_s, img.to_s]
-      puts ["#{name&.text} - #{w&.text}", price[index]&.text.to_s, img.to_s]
     end
   end
 end
@@ -29,9 +28,9 @@ def product(html_body)
   save_info(name, weight, img, price)
 end
 
-products_url.each do |url|
-  current_url = Curl.get(url.text)
-  current_html = Nokogiri::HTML(current_url.body)
-  product(current_html)
+products_data.each do |uri|
+  product_link = Curl.get(uri.text)
+  product_html = Nokogiri::HTML(product_link.body)
+  product(product_html)
 end
 puts 'Все файлы записаны'

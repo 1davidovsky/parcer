5ca53445d8d2fae44739f5726a407d3375decf8c
aleksandr <an5600590@gmail.com>
2021-10-13 15:36:44 +0300

    record.1

--- parser.rb
+++ parser.rb
@@ -10,22 +10,22 @@ http = Curl.get(link)
 html = Nokogiri::HTML(http.body)
 products_data = html.xpath("//div/div/link[@itemprop='url']/@href")
 
-def save_info(name, weight, img, price)
-  weight.each_with_index do |w, index|
+def save_info(name, weight_total, img, price)
+  weight_total.each_with_index do |weight, index|
     CSV.open('test.csv', 'a+') do |csv_file|
       csv_file << %w[Название Цена Вес] if csv_file.count.zero?
-      csv_file << ["#{name&.text} - #{w&.text}", price[index]&.text.to_s, img.to_s]
+      csv_file << ["#{name&.text} - #{weight&.text}", price[index]&.text.to_s, img.to_s]
     end
   end
 end
 
 def product(html_body)
   name = html_body.xpath('//h1[@class="product_main_name"]')
-  weight = html_body.xpath('//span[@class="radio_label"]')
+  weight_total = html_body.xpath('//span[@class="radio_label"]')
   price = html_body.xpath('//span[@class="price_comb"]')
   img = html_body.xpath('//img[@id="bigpic"]/@src')
 
-  save_info(name, weight, img, price)
+  save_info(name, weight_total, img, price)
 end
 
 products_data.each do |uri|
